b60eb276edbb102db03874b2bdf04526f842539d
reduviidae <wiljago@gmail.com>
2019-02-19 11:46:07 -0500

    finished

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import "./App.css";
+import Header from "./Components/Header";
 
 class App extends Component {
   render() {
-    return <header />;
+    return <Header title="Title"/>;
   }
 }
 
